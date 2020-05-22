402729b7b0a20790c4793eb70a3c5ff1d56072b1
astrovskiy-199 <astrovskiy.a@gmail.com>
2020-05-22 16:43:33 +0300

    AboutMeAdaptiveDesign_update

--- about_me/script/script.js
+++ about_me/script/script.js
@@ -1,5 +1,5 @@
 var ButtonTop =document.getElementById("ButtonTop");
-var count = 0
+var countCharity = 0
 var count_of_IMG =0 
 var image_viewBlock_button_left = document.getElementById("imageViewBlockButtonLeft")
 var image_viewBlock_button_right = document.getElementById("imageViewBlockButtonRight")
@@ -16,49 +16,99 @@ window.addEventListener('scroll', function() {
     
   });
 
+  var dinophoto= document.getElementsByClassName('dinophoto') 
+  for (i=0; i < dinophoto.length; i++) { 
+    dinophoto[0].addEventListener('click', function(){  count_of_IMG = 5
+        if(count_of_IMG === 5) {
+            image_viewBlock.style.display="flex"; image_viewBlock.style.backgroundImage="url('../img/bigPhotoDinoThree.jpg')";  image_viewBlock_button_left.style.display="none"; console.log(count_of_IMG ) ; image_viewBlock_button_right.style.display="flex";
+              
+        }
+        
+        
+
+        
+      });
+
+      dinophoto[1].addEventListener('click', function(){  count_of_IMG = 6
+        if(count_of_IMG === 6) {
+            image_viewBlock.style.display="flex"; image_viewBlock.style.backgroundImage="url('../img/bigPhotoDinoThree.jpg')";  image_viewBlock_button_left.style.display="flex"; console.log(count_of_IMG ) ; image_viewBlock_button_right.style.display="flex";
+              
+        }
+        
+        
+
+        
+      });
+
+
+      dinophoto[2].addEventListener('click', function(){  count_of_IMG = 7
+        if(count_of_IMG === 7) {
+            image_viewBlock.style.display="flex"; image_viewBlock.style.backgroundImage="url('../img/bigPhotoDinoOne.jpg')";  image_viewBlock_button_left.style.display="flex"; console.log(count_of_IMG ) ; image_viewBlock_button_right.style.display="flex";
+              
+        }
+
+        
+      });
+
+
+      dinophoto[3].addEventListener('click', function(){  count_of_IMG = 8
+        if(count_of_IMG === 8) {
+            image_viewBlock.style.display="flex"; image_viewBlock.style.backgroundImage="url('../img/bigPhotoDinoFourth.jpg')";  image_viewBlock_button_left.style.display="flex"; console.log(count_of_IMG ) ; image_viewBlock_button_right.style.display="none";
+              
+        }
+
+        
+      });
+
+
+
+      
+  }
+  
+
    var BigPhoto= document.getElementsByClassName('seventhBlockIconsFractionIcon');
     for( i = 0; i < BigPhoto.length; i++){ 
-        BigPhoto[0].addEventListener('click', function() { count++
+        BigPhoto[0].addEventListener('click', function() { count_of_IMG = 0
            
             
-            if(count > 1) {
+            if(count_of_IMG === 0) {
                 image_viewBlock.style.display="flex" ; image_viewBlock.style.backgroundImage="url('../img/qa-course-one-small.jpg')";image_viewBlock_button_left.style.display="none"; count_of_IMG = 0; image_viewBlock_button_right.style.display="flex";
             }
             
         })
 
-        BigPhoto[1].addEventListener('click', function() { count++
+        BigPhoto[1].addEventListener('click', function() { count_of_IMG = 1
            
             
-            if(count > 1) {
+            if(count_of_IMG === 1) {
                 image_viewBlock.style.display="flex" ; image_viewBlock.style.backgroundImage="url('../img/qa-course-two-small.jpg')"; image_viewBlock_button_right.style.display="flex";  image_viewBlock_button_left.style.display="flex";
             }
   
         })
 
 
-        BigPhoto[2].addEventListener('click', function() { count++
+        BigPhoto[2].addEventListener('click', function() { count_of_IMG = 2
            
             
-            if(count > 1) {
+            if(count_of_IMG === 2) {
                 image_viewBlock.style.display="flex" ; image_viewBlock.style.backgroundImage="url('../img/qa-course-three-small.jpg')"; image_viewBlock_button_right.style.display="flex";  image_viewBlock_button_left.style.display="flex";
             }
   
         })
 
-        BigPhoto[3].addEventListener('click', function() { count++
+        BigPhoto[3].addEventListener('click', function() { count_of_IMG = 3
            
             
-            if(count > 1) {
+            if(count_of_IMG === 3) {
                 image_viewBlock.style.display="flex" ; image_viewBlock.style.backgroundImage="url('../img/qa-course-forth-small.jpg')"; image_viewBlock_button_right.style.display="flex";  image_viewBlock_button_left.style.display="flex";
             }
   
         })
 
-        BigPhoto[4].addEventListener('click', function() { count++
+        BigPhoto[4].addEventListener('click', function() { count_of_IMG = 4
            
             
-            if(count > 1) {
+            if(count_of_IMG === 4) {
                 image_viewBlock.style.display="flex" ; image_viewBlock.style.backgroundImage="url('../img/qa-course-fifth-small.jpg')"; image_viewBlock_button_right.style.display="none"; count_of_IMG = 4; image_viewBlock_button_left.style.display="flex";
             }
   
@@ -73,6 +123,8 @@ window.addEventListener('scroll', function() {
             image_viewBlock_button_left.style.display="none"; image_viewBlock.style.backgroundImage="url('../img/qa-course-one-small.jpg')"; 
             
         }
+
+        
 
         
 
@@ -92,12 +144,31 @@ window.addEventListener('scroll', function() {
         if(count_of_IMG === 4) {
             image_viewBlock.style.backgroundImage="url('../img/qa-course-fifth-small.jpg')"; 
         }
+
+        if(count_of_IMG === 5) {
+            image_viewBlock.style.display="flex"; image_viewBlock.style.backgroundImage="url('../img/bigPhotoDinoThree.jpg')";  image_viewBlock_button_left.style.display="none"; console.log(count_of_IMG ); image_viewBlock_button_right.style.display="flex";
+        }
+       
+
+        if(count_of_IMG === 6) {
+            image_viewBlock.style.display="flex"; image_viewBlock.style.backgroundImage="url('../img/bigPhotoDinoTwo.jpg')";  image_viewBlock_button_left.style.display="flex"; console.log(count_of_IMG ); image_viewBlock_button_right.style.display="flex";
+        }
+
+        if(count_of_IMG === 7) {
+            image_viewBlock.style.display="flex"; image_viewBlock.style.backgroundImage="url('../img/bigPhotoDinoOne.jpg')";  image_viewBlock_button_left.style.display="flex"; console.log(count_of_IMG ); image_viewBlock_button_right.style.display="flex";
+        }
+
+        if(count_of_IMG === 8) {
+            image_viewBlock.style.display="flex"; image_viewBlock.style.backgroundImage="url('../img/bigPhotoDinoFourth.jpg')";  image_viewBlock_button_left.style.display="flex"; console.log(count_of_IMG ); image_viewBlock_button_right.style.display="none";
+        }
     })
 
         
 
     image_viewBlock_button_right.addEventListener('click', function(){
-        count_of_IMG++
+        count_of_IMG++; console.log(count_of_IMG );
+
+
         if(count_of_IMG === 1) {
             image_viewBlock.style.backgroundImage="url('../img/qa-course-two-small.jpg')"; image_viewBlock_button_left.style.display="flex";
         }
@@ -114,9 +185,26 @@ window.addEventListener('scroll', function() {
             image_viewBlock.style.backgroundImage="url('../img/qa-course-fifth-small.jpg')"; image_viewBlock_button_right.style.display="none";
         }
 
-        if(count_of_IMG > 4) { 
-            count_of_IMG = 0;
+        if(count_of_IMG === 5) {
+            image_viewBlock.style.display="flex"; image_viewBlock.style.backgroundImage="url('../img/bigPhotoDinoTwo.jpg')";  image_viewBlock_button_left.style.display="none"; console.log(count_of_IMG ); image_viewBlock_button_right.style.display="flex";
         }
+       
+        if(count_of_IMG === 6) {
+            image_viewBlock.style.display="flex"; image_viewBlock.style.backgroundImage="url('../img/bigPhotoDinoTwo.jpg')";  image_viewBlock_button_left.style.display="flex"; console.log(count_of_IMG ); image_viewBlock_button_right.style.display="flex";
+        }
+
+        if(count_of_IMG === 7) {
+            image_viewBlock.style.display="flex"; image_viewBlock.style.backgroundImage="url('../img/bigPhotoDinoOne.jpg')";  image_viewBlock_button_left.style.display="flex"; console.log(count_of_IMG ); image_viewBlock_button_right.style.display="flex";
+        }
+
+        if(count_of_IMG === 8) {
+            image_viewBlock.style.display="flex"; image_viewBlock.style.backgroundImage="url('../img/bigPhotoDinoFourth.jpg')";  image_viewBlock_button_left.style.display="flex"; console.log(count_of_IMG ); image_viewBlock_button_right.style.display="none";
+        }
+
+
+      
+
+        
 
     
        

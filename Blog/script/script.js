41f09febb4a8d8aff9623460b1fd3307dacb280d
astrovskiy-199 <astrovskiy.a@gmail.com>
2020-06-18 00:21:24 +0300

    NewUpdatesPageBlog

--- Blog/script/script.js
+++ Blog/script/script.js
@@ -1,9 +1,7 @@
 var mainBlock = document.getElementById("mainBlock");
 var loader = document.getElementById("loader");
 var ButtonTop =document.getElementById("ButtonTop");
-var ShowImageBlock = document.getElementById("ShowImageBlock");
-var ShowImageBlockClose = document.getElementById('ShowImageBlockClose')
-var count_of_IMG = 0;
+var ArticlesList  = document.getElementsByClassName('ArticlesList')
 
 window.addEventListener('scroll', function() {
     if (pageYOffset > 1030) {
@@ -16,32 +14,13 @@ window.addEventListener('scroll', function() {
     
   });
 
-var ShowImdByClick = document.getElementsByClassName('ExampleBlockImg'); 
-for(i=0; i < ShowImdByClick.length; i++); 
-ShowImdByClick[0].addEventListener('click', function() {
-    ShowImageBlock.style.display="flex";
-    ShowImageBlock.style.backgroundImage="url(../skills/img/TestCase.jpg)";
+  for ( i = 0; i < ArticlesList.length; i++) {
+      
+      
+      
 
-} ) ;
+  }
 
-ShowImdByClick[1].addEventListener('click', function() {
-    ShowImageBlock.style.display="flex";
-    ShowImageBlock.style.backgroundImage="url(../skills/img/TestReport.jpg)";
-
-} ) ;
-
-ShowImdByClick[2].addEventListener('click', function() {
-    ShowImageBlock.style.display="flex";
-    ShowImageBlock.style.backgroundImage="url(../skills/img/estimation.jpg)";
-
-} ) 
-
-
-
-
-ShowImageBlockClose.addEventListener('click', function() {
-    ShowImageBlock.style.display="none";
-})
 
 
 

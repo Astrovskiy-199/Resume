b867a0332bb8d09bff339da7eec4a635b60a372c
astrovskiy-199 <astrovskiy.a@gmail.com>
2020-06-21 02:05:52 +0300

    ArticlesAdded

--- Blog/script/script.js
+++ Blog/script/script.js
@@ -2,6 +2,11 @@ var mainBlock = document.getElementById("mainBlock");
 var loader = document.getElementById("loader");
 var ButtonTop =document.getElementById("ButtonTop");
 var ArticlesList  = document.getElementsByClassName('ArticlesList')
+var ShowArticle = document.getElementsByClassName('ShowArticle')
+var CloseArticleButton = document.getElementsByClassName('CloseArticleButton')
+var ArticlesCount = 0
+
+
 
 window.addEventListener('scroll', function() {
     if (pageYOffset > 1030) {
@@ -14,12 +19,80 @@ window.addEventListener('scroll', function() {
     
   });
 
-  for ( i = 0; i < ArticlesList.length; i++) {
-      
-      
-      
 
-  }
+
+for(i=0; i < ArticlesList.length; i++) {  
+
+}
+
+for(q=0; q< ShowArticle.length; q++) {
+
+}
+
+for(n=0; n < CloseArticleButton.length; n++) { 
+
+}
+
+
+
+ArticlesList[0].addEventListener('click', function() { ArticlesCount = 1
+    ShowArticle[0].style.display="flex";
+ 
+   
+    
+     
+})
+
+CloseArticleButton[0].addEventListener('click', function() { ArticlesCount = 1
+    ShowArticle[0].style.display="none";
+     
+})
+
+
+
+ArticlesList[1].addEventListener('click', function() { ArticlesCount = 1
+    ShowArticle[1].style.display="flex";
+     
+})
+
+CloseArticleButton[1].addEventListener('click', function() { ArticlesCount = 1
+    ShowArticle[1].style.display="none";
+     
+})
+
+
+
+ArticlesList[2].addEventListener('click', function() { ArticlesCount = 1
+    ShowArticle[2].style.display="flex";
+     
+})
+
+CloseArticleButton[2].addEventListener('click', function() { ArticlesCount = 1
+    ShowArticle[2].style.display="none";
+     
+})
+
+
+
+ArticlesList[3].addEventListener('click', function() { ArticlesCount = 1
+    ShowArticle[3].style.display="flex";
+     
+})
+
+CloseArticleButton[3].addEventListener('click', function() { ArticlesCount = 1
+    ShowArticle[3].style.display="none";
+     
+})
+
+
+
+
+
+
+
+
+
+
 
 
 
